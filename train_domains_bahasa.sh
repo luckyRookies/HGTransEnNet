dbbe6762cc4fc3e16f08dad10bac14658b76ac8d
kewenkang <1528428728@qq.com>
2020-01-13 17:14:06 +0800

    add nohup training bash

--- train_domains_bahasa.sh
+++ train_domains_bahasa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(attraction hotel restaurant taxi)
+domains=(taxi)
 
 for d in ${domains[*]};
 do
-  nohup sh run_bahasa/bahasa_with_bert.sh ${d} > ${d}_log.txt 2>&1 &
+  nohup sh run_bahasa/bahasa_with_bert.sh ${d} > logs/${d}_log.txt 2>&1 &
 done
 
